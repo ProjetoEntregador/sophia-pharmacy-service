52bb4c5840b9043e1153e7ebdcb1efbf77ba7b1f
thiagomoraiis <thiagomorais2605@gmail.com>
2026-04-20 20:06:08 -0300

    first commit

---
+++ src/test/java/com/sophia/sophia_pharmacy_service/SophiaPharmacyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophia.sophia_pharmacy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SophiaPharmacyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
